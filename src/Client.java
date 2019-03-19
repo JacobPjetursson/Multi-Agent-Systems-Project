0f2e8ececbfe65c223c67183131fc45dfcc49a31
matias <matias@matiass-mbp.eduroam.wireless.dtu.dk>
2019-03-19 17:02:45 +0100

    New/Updated levels

--- src/Client.java
+++ src/Client.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Queue;
 public class Client {
 
 	private static String domain;
