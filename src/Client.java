1e417bb52e6719ebd165eeb4b78f0d06bf91b915
MatiasDaugaard <matiasdaugaard@gmail.com>
2019-03-19 14:18:02 +0100

    Main ready

--- src/Client.java
+++ src/Client.java
@@ -1,6 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 public class Client {
+    
 
-    public static void main(String[] args) {
-        System.out.println("ExampleClient");
+    public static void main(String[] args) throws Exception {
+        BufferedReader serverMessages = new BufferedReader(new InputStreamReader(System.in));
+        
+        // STEP 1 : First System.out.println(client name);
+        System.out.println("Client name");
+        
+        // STEP 2 : Read level file from server;
+        String response = serverMessages.readLine();
+        while (!response.contains("end")) {
+            //Load the level
+            response = serverMessages.readLine();
+        }
+        
+        // STEP 3 : System.out.println(Solution);
+
     }
 }
